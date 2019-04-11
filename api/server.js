57bbd67e8c4292872e17eb18087d2b22883af71a
korynewton <korynewtondev@gmail.com>
2019-04-11 15:51:29 -0700

    POST route made in server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const Chars = require('../models/models');
 
 const server = express();
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.json({ message: 'Get working' });
+});
+
+server.post('/add', async (req, res) => {
+  try {
+    const character = await Chars.insert(req.body);
+    res.status(200).json(character);
+  } catch {
+    res.status(500).json({ message: 'error' });
+  }
 });
 
 module.exports = server;
