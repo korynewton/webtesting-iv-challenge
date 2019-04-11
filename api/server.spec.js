def4ba204f2441aa5c7f53e32ff07f418b19fd60
korynewton <korynewtondev@gmail.com>
2019-04-11 13:36:21 -0700

    project initialized, databasse added

---
+++ api/server.spec.js
@@ -0,0 +1,5 @@
+const request = require('supertest');
+
+describe('server', () => {
+  describe('GET /', () => {});
+});
---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require('./api');
+
+server.listen(5000, () => {
+  consoile.log('\n**up and running on 5k');
+});
